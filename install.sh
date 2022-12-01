3ccc33213d05b3d9b39960323eb4de969ac76ee2
Lucas Chaves <y3tsec@protonmail.com>
2022-12-01 11:36:16 -0300

    install.sh builds the program as release now

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 #This program needs cargo and rust installed
 #It only works in Unix Systems
 #I am pretty sure there are better ways to make this work. For now this is fine.
-cargo build
+cargo build --release
 mv target/release/ghostlyfetch /bin/
